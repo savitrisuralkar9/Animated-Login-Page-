6aa620f0c5e6cac44d62092364f852682f85697a
savitrisuralkar9 <savitrisuralkar9@gmail.com>
2026-01-30 22:29:27 +0530

    Add click event to move login box

---
+++ script.js
@@ -0,0 +1,6 @@
+const girl = document.getElementById("girl");
+const loginBox = document.getElementById("loginBox");
+
+girl.addEventListener("click", () => {
+  loginBox.style.left = "350px"; // move to center
+});
